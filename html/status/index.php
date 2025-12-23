039c25f7befd4502092a7cf0585d37fda773a4de
Adam Blomfield <ablomfield@presidio.com>
2025-12-22 22:35:18 -0600

    optimized

--- html/status/index.php
+++ html/status/index.php
@@ -1,87 +1,146 @@
 <?php
 // Retrieve Settings and Functions
 include($_SERVER['DOCUMENT_ROOT'] . "/includes/settings.php");
+
 $officecount = 0;
-echo ("			<table class=\"default\">\n");
-echo ("				<thead>\n");
-echo ("					<tr>\n");
-echo ("						<th>Room</th>\n");
-echo ("						<th>In Use</th>\n");
-echo ("						<th>Count</th>\n");
-echo ("					</tr>\n");
-echo ("				</thead>\n");
-echo ("				<tbody>\n");
+
+// -------------------------------------------------
+// Load devices from DB
+// -------------------------------------------------
+$devices = [];
 $devsql = "SELECT deviceid, spacename FROM spaces";
 $rsdev = $dbconn->query($devsql);
+
 if ($rsdev->num_rows > 0) {
-    // Output data of each row
     while ($row = $rsdev->fetch_assoc()) {
-        echo ("					<tr>\n");
-        echo ("						<td>" . $row["spacename"] . "</td>\n");
-        $deviceurl = "https://webexapis.com/v1/xapi/status/?deviceId=" . $row["deviceid"] . "&name=RoomAnalytics.RoomInUse";
-        //echo $deviceurl;
-        $getdevice = curl_init($deviceurl);
-        curl_setopt($getdevice, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($getdevice, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt(
-            $getdevice,
-            CURLOPT_HTTPHEADER,
-            array(
-                'Content-Type: application/json',
-                'Authorization: Bearer ' . $accesstoken
-            )
-        );
-        $devicejson = curl_exec($getdevice);
-        $devicearray = json_decode($devicejson);
-        //print_r($devicejson);
-        if (isset($devicearray->result->RoomAnalytics->RoomInUse)) {
-            if ($devicearray->result->RoomAnalytics->RoomInUse == "True") {
-                $roominuse = true;
-                echo ("						<td bgcolor=\"green\">" . $devicearray->result->RoomAnalytics->RoomInUse . "</td>\n");
-            } else {
-                $roominuse = false;
-                echo ("						<td>" . $devicearray->result->RoomAnalytics->RoomInUse . "</td>\n");
-            }
-        } else {
-            echo ("						<td>False</td>\n");
-        }
-        $deviceurl = "https://webexapis.com/v1/xapi/status/?deviceId=" . $row["deviceid"] . "&name=RoomAnalytics.PeopleCount.Current";
-        //echo $deviceurl;
-        $getdevice = curl_init($deviceurl);
-        curl_setopt($getdevice, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($getdevice, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt(
-            $getdevice,
-            CURLOPT_HTTPHEADER,
-            array(
-                'Content-Type: application/json',
-                'Authorization: Bearer ' . $accesstoken
-            )
-        );
-        $devicejson = curl_exec($getdevice);
-        $devicearray = json_decode($devicejson);
-        //print_r($devicejson);
-        if (isset($devicearray->result->RoomAnalytics->PeopleCount->Current)) {
-            if ($devicearray->result->RoomAnalytics->PeopleCount->Current > 0) {
-                $officecount = $officecount + $devicearray->result->RoomAnalytics->PeopleCount->Current;
-                echo ("						<td bgcolor=\"green\" align=\"center\">" . $devicearray->result->RoomAnalytics->PeopleCount->Current . "</td>\n");
-            } else {
-                echo ("						<td align=\"center\">0</td>\n");
-            }
-        } elseif ($roominuse) {
-            $officecount = $officecount + 1;
-            echo ("						<td bgcolor=\"green\" align=\"center\">1</td>\n");
-        } else {
-            echo ("						<td align=\"center\">-</td>\n");
-        }
-        echo ("					</tr>\n");
+        $devices[$row['deviceid']] = [
+            'spacename' => $row['spacename'],
+            'inuse'     => false,
+            'count'     => null
+        ];
     }
 }
-echo ("				</tbody>\n");
-echo ("				<thead>\n");
-echo ("					<tr>\n");
-echo ("						<th colspan=\"2\">Total</th>\n");
-echo ("						<th>$officecount</th>\n");
-echo ("					</tr>\n");
-echo ("				</thead>\n");
-echo ("			</table>\n");
+
+// -------------------------------------------------
+// Prepare parallel cURL requests
+// -------------------------------------------------
+$mh = curl_multi_init();
+$handles = [];
+
+$headers = [
+    'Content-Type: application/json',
+    'Authorization: Bearer ' . $accesstoken
+];
+
+foreach ($devices as $deviceid => $data) {
+
+    // Room In Use
+    $urlInUse = "https://webexapis.com/v1/xapi/status/?deviceId={$deviceid}&name=RoomAnalytics.RoomInUse";
+    $ch1 = curl_init($urlInUse);
+    curl_setopt_array($ch1, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER     => $headers
+    ]);
+    curl_multi_add_handle($mh, $ch1);
+    $handles[(string)$ch1] = ['deviceid' => $deviceid, 'type' => 'inuse'];
+
+    // People Count
+    $urlCount = "https://webexapis.com/v1/xapi/status/?deviceId={$deviceid}&name=RoomAnalytics.PeopleCount.Current";
+    $ch2 = curl_init($urlCount);
+    curl_setopt_array($ch2, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER     => $headers
+    ]);
+    curl_multi_add_handle($mh, $ch2);
+    $handles[(string)$ch2] = ['deviceid' => $deviceid, 'type' => 'count'];
+}
+
+// -------------------------------------------------
+// Execute all requests in parallel
+// -------------------------------------------------
+do {
+    $status = curl_multi_exec($mh, $active);
+    if ($active) {
+        curl_multi_select($mh);
+    }
+} while ($active && $status == CURLM_OK);
+
+// -------------------------------------------------
+// Process responses
+// -------------------------------------------------
+foreach ($handles as $handleKey => $meta) {
+    $ch = array_search($handleKey, array_map('strval', array_keys($handles))) !== false
+        ? array_keys($handles)[array_search($handleKey, array_map('strval', array_keys($handles)))]
+        : null;
+
+    if (!$ch) continue;
+
+    $response = curl_multi_getcontent($ch);
+    $json = json_decode($response);
+
+    $deviceid = $meta['deviceid'];
+
+    if ($meta['type'] === 'inuse') {
+        if (isset($json->result->RoomAnalytics->RoomInUse) &&
+            $json->result->RoomAnalytics->RoomInUse === "True") {
+            $devices[$deviceid]['inuse'] = true;
+        }
+    }
+
+    if ($meta['type'] === 'count') {
+        if (isset($json->result->RoomAnalytics->PeopleCount->Current)) {
+            $devices[$deviceid]['count'] = (int)$json->result->RoomAnalytics->PeopleCount->Current;
+        }
+    }
+
+    curl_multi_remove_handle($mh, $ch);
+    curl_close($ch);
+}
+
+curl_multi_close($mh);
+
+// -------------------------------------------------
+// Render HTML
+// -------------------------------------------------
+echo "<table class=\"default\">\n";
+echo "<thead>
+        <tr>
+            <th>Room</th>
+            <th>In Use</th>
+            <th>Count</th>
+        </tr>
+      </thead>\n<tbody>\n";
+
+foreach ($devices as $device) {
+    echo "<tr>";
+    echo "<td>{$device['spacename']}</td>";
+
+    // In Use column
+    if ($device['inuse']) {
+        echo "<td bgcolor=\"green\">True</td>";
+    } else {
+        echo "<td>False</td>";
+    }
+
+    // Count column
+    if ($device['count'] !== null && $device['count'] > 0) {
+        $officecount += $device['count'];
+        echo "<td bgcolor=\"green\" align=\"center\">{$device['count']}</td>";
+    } elseif ($device['inuse']) {
+        $officecount += 1;
+        echo "<td bgcolor=\"green\" align=\"center\">1</td>";
+    } else {
+        echo "<td align=\"center\">-</td>";
+    }
+
+    echo "</tr>\n";
+}
+
+echo "</tbody>
+      <thead>
+        <tr>
+            <th colspan=\"2\">Total</th>
+            <th>{$officecount}</th>
+        </tr>
+      </thead>
+      </table>\n";
